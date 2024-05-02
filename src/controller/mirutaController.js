1c1e905b9ac89724c6ddfa14075ab911075fba94
Alexbpx <Alexbulnespovea@gmail.com>
2024-05-02 12:00:32 +0200

    add productList

---
+++ src/controller/mirutaController.js
@@ -0,0 +1,18 @@
+const getIndex = (req, res) => {
+    res.render('index');
+};
+const getLogin = (req, res) => {
+    res.render('login');
+};
+const getSignUp = (req, res) => {
+    res.render('signUp');
+};
+const getProductList = (req, res) => {
+    res.render('productList');
+};
+module.exports = {
+    getIndex,
+    getLogin,
+    getSignUp,
+    getProductList,
+};
---
+++ src/routes/miruta.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const mirutaController = require('../controller/mirutaController');
+router
+    .get('/' ,mirutaController.getIndex)
+    .get('/login' ,mirutaController.getLogin)
+    .get('/signUp' ,mirutaController.getSignUp)
+    .get('/productList' ,mirutaController.getProductList);
+
+module.exports = router;
