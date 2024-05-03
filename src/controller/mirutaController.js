2f30261d2453c5d6be3846332d5c1fd498cbabc6
Alexbpx <Alexbulnespovea@gmail.com>
2024-05-03 14:37:58 +0200

    especific productlist

--- src/controller/mirutaController.js
+++ src/controller/mirutaController.js
@@ -12,10 +12,39 @@ const getProductList = (req, res) => {
     res.render('productList');
 };
 
+const getDunk1 = (req, res) => {
+    res.render('dunk1');
+};
+
+const getDunk2 = (req, res) => {
+    res.render('dunk2');
+};
+
+const getDunk3 = (req, res) => {
+    res.render('dunk3');
+};
+
+const getDunk4 = (req, res) => {
+    res.render('dunk4');
+};
+
+const getDunk5 = (req, res) => {
+    res.render('dunk5');
+};
+
+const getDunk6 = (req, res) => {
+    res.render('dunk6');
+};
+
 module.exports = {
     getIndex,
     getLogin,
     getSignUp,
-
     getProductList,
+    getDunk1,
+    getDunk2,
+    getDunk3,
+    getDunk4,
+    getDunk5,
+    getDunk6,
 };
--- src/routes/miruta.js
+++ src/routes/miruta.js
@@ -7,6 +7,12 @@ router
     .get('/login' ,mirutaController.getLogin)
     .get('/signUp' ,mirutaController.getSignUp)
     .get('/signUp' ,mirutaController.getSignUp)
-    .get('/productList' ,mirutaController.getProductList);
+    .get('/productList' ,mirutaController.getProductList)
+    .get('/productList/dunk1' ,mirutaController.getDunk1)
+    .get('/productList/dunk2' ,mirutaController.getDunk2)
+    .get('/productList/dunk3' ,mirutaController.getDunk3)
+    .get('/productList/dunk4' ,mirutaController.getDunk4)
+    .get('/productList/dunk5' ,mirutaController.getDunk5)
+    .get('/productList/dunk6' ,mirutaController.getDunk6);
 
 module.exports = router;
