4894c90ba81ecbf418391ae76ad88e8897a686e7
javimr1224 <javimr1224@gmail.com>
2024-05-06 17:26:48 +0200

    Revert "Merge branch 'feature2' into develop"
    This reverts commit 95492ca770cae33176b2a0b0e0ae57cd75aa6021, reversing
    changes made to 34d8aee672b2b8d6386957006bb66f24b2c72b9c.

--- src/Js/darkmode.js
+++
@@ -1,7 +0,0 @@
-var toggle = document.getElementById('container-darkMode');
-var body = document.querySelector('body');
-
-toggle.onclick = function(){
-    toggle.classList.toggle('active');
-    body.classList.toggle('active');
-}
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+//importamos las rutas
+//const loginRoutes = require('./routes/login');
+//const imageRoutes = require('./routes/image');
 const miruta = require('./routes/miruta');
 const app = express();
 const port = 3000;
@@ -15,17 +18,15 @@ app.use(session({
 }));
 
 // Configuración para servir archivos estáticos
-app.use(express.static('public'));
-
-// Plantillas
+app.use(express.static(__dirname));
+//plantillas
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
+// Routes
+//app.use('/', loginRoutes);
+//app.use('/', imageRoutes);
+app.use('/' , miruta);
 
-// Rutas
-// Aquí puedes agregar más rutas si es necesario
-app.use('/', miruta);
-
-// Iniciar el servidor
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
