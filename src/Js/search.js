f80628454aa04593360467cb4757e56c5c6946e7
Alecaroo <138302152+Alecaroo@users.noreply.github.com>
2024-05-06 22:07:58 +0200

    Update search.js

--- src/Js/search.js
+++ src/Js/search.js
@@ -2,16 +2,17 @@ function search() {
     // Obtener el término de búsqueda
     var searchTerm = document.getElementById('searchInput').value.toLowerCase();
 
-    // Obtener todos los elementos a buscar
-    var elements = document.getElementsByClassName('element');
+    // Obtener todos los nombres de productos
+    var productNames = document.querySelectorAll('.titulo-producto-carrito');
 
-    // Iterar sobre los elementos y ocultar los que no coincidan con el término de búsqueda
-    for (var i = 0; i < elements.length; i++) {
-        var elementName = elements[i].innerText.toLowerCase();
-        if (elementName.includes(searchTerm)) {
-            elements[i].style.display = 'block'; // Mostrar elementos que coincidan
+    // Iterar sobre los nombres de productos y mostrar/ocultar los elementos que coincidan/no coincidan
+    productNames.forEach(function(product) {
+        var productName = product.innerText.toLowerCase();
+        var productElement = product.closest('.cart-product'); // Obtener el elemento contenedor del producto
+        if (productName.includes(searchTerm)) {
+            productElement.style.display = 'flex'; // Mostrar el contenedor del producto
         } else {
-            elements[i].style.display = 'none'; // Ocultar elementos que no coincidan
+            productElement.style.display = 'none'; // Ocultar el contenedor del producto
         }
-    }
+    });
 }
