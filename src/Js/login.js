69b79fb10d5cd77970f31fca69db7382003bd7eb
javimr1224 <javimr1224@gmail.com>
2024-05-06 22:55:51 +0200

    login and register

--- src/Js/login.js
+++ src/Js/login.js
@@ -1,7 +1,8 @@
-const mensajeError = document.getElementsByClassName("error")[0]
+const mensajeError = document.getElementsByClassName("error")[0];
 
 document.getElementById("login-form").addEventListener("submit",async (e)=>{
   e.preventDefault();
+  console.log(e.target.children.user.value)
   const user = e.target.children.user.value;
   const password = e.target.children.password.value;
   const res = await fetch("http://localhost:3000/api/login",{
