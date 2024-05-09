049ebea2cb4558490992e87ad7b30e4dcdfbf52f
javimr1224 <javimr1224@gmail.com>
2024-05-09 14:11:45 +0200

    login y register works milestone3

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 //importamos las rutas
-//const loginRoutes = require('./routes/login');
-//const imageRoutes = require('./routes/image');
+const loginRoutes = require('./routes/login');
 const miruta = require('./routes/miruta');
+
 const app = express();
 const port = 3000;
 
@@ -23,8 +23,7 @@ app.use(express.static(__dirname));
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 // Routes
-//app.use('/', loginRoutes);
-//app.use('/', imageRoutes);
+app.use('/' , loginRoutes);
 app.use('/' , miruta);
 
 app.listen(port, () => {
